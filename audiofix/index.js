408059b1962506a36e84dbfa320b38ec0297f1c2
Narwhal Kid <madgamer2525@gmail.com>
2024-11-15 20:15:12 -0500

    g

---
+++ audiofix/index.js
@@ -0,0 +1,1 @@
+(function(t,e,n){"use strict";const a=n.instead("setCommunicationModeOn",e.ReactNative.NativeModules.VoiceEngine===null?e.ReactNative.NativeModules.RTNAudioManager:e.ReactNative.NativeModules.VoiceEngine,function(){});return t.onUnload=a,t})({},vendetta.metro.common,vendetta.patcher);
