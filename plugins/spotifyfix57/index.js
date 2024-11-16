ffb88dc39110afc43fe47af2de36867dbea418f5
Narwhal Kid <madgamer2525@gmail.com>
2024-11-15 19:58:13 -0500

    g

--- plugins/spotifyfix57/index.js
+++ plugins/spotifyfix57/index.js
@@ -4,23 +4,30 @@
     const { before } = patcher;
     const { findByProps } = metro;
     const SpotifyStore = findByProps("dispatch");
-    const { logger } = vendetta
-    
+    const { logger } = vendetta;
+
+    let unpatch; // Variable to hold the unpatch function
 
     const patch = () => {
-        console.log("hi!");
-        before("dispatch", SpotifyStore, ([action]) => {
-            console.log(JSON.stringify(action));
-            console.log(action);
+        unpatch = before("dispatch", SpotifyStore, ([action]) => {
             if (
                 action.type === "SPOTIFY_PROFILE_UPDATE" &&
                 action.payload?.isPremium === undefined
             ) {
                 action.payload.isPremium = true; // Force premium
             }
-        })
-    }
+        });
+    };
 
+    // Called when the plugin is loaded
     context.onLoad = patch;
 
-})({}, vendetta.patcher, vendetta.metro, vendetta);
+    // Called when the plugin is unloaded
+    context.onUnload = () => {
+        if (typeof unpatch === "function") {
+            unpatch(); // Unpatch the modification
+            logger.log("Unpatched successfully!");
+        }
+    };
+
+})(vendetta.context, vendetta.patcher, vendetta.metro, vendetta);
--- plugins/test2/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { General } from "@vendetta/ui/components";
-
-const { Text } = General;
-
-export const onLoad = () => console.log("Plugin loaded!");
-export const onUnload = () => console.log("Plugin loaded!");
-export const Settings = () => <Text style={{ color: "#000" }}>Hello!</Text>;
