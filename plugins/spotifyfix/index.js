87e2e919217cdbc63fe53994d28a497a4708b82c
Narwhal Kid <madgamer2525@gmail.com>
2024-11-16 09:52:54 -0500

    Update index.js

--- plugins/spotifyfix/index.js
+++ plugins/spotifyfix/index.js
@@ -1,5 +1,4 @@
 import { findByProps } from "@vendetta/metro";
-import { logger } from "@vendetta";
 import { before } from "@vendetta/patcher";
 
 const SpotifyStore = findByProps("dispatch")
@@ -8,26 +7,19 @@ let unpatch
 export default {
 
     onLoad: () => {
-        console.log('hey!')
-        logger.log("Hey!");
         unpatch = before("dispatch", SpotifyStore, ([action]) => {
-            console.log(JSON.stringify(action));
-            logger.log(JSON.stringify(action));
             if (
                 action.type === "SPOTIFY_PROFILE_UPDATE" &&
                 action.payload?.isPremium === undefined
             ) {
-                action.payload.isPremium = true; // Force premium
+                action.payload.isPremium = true;
             }
         });
     },
 
-    // Called when the plugin is unloaded
     onUnload: () => {
         if (typeof unpatch === "function") {
-            unpatch(); // Unpatch the modification
-            console.log('unpatched!')
-            logger.log("Unpatched successfully!");
+            unpatch();
         }
     },
 }
