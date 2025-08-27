ca93785e3eda7c824099c2fd5f0fc62648185d68
Narwhal Kid <madgamer2525@gmail.com>
2025-08-27 04:42:13 -0400

    fix for 294.18

--- plugins/audiofix/index.js
+++ plugins/audiofix/index.js
@@ -1,4 +1,4 @@
 import { ReactNative as RN } from "@vendetta/metro/common";
 import { instead } from "@vendetta/patcher";
 
-export const onUnload = instead("setCommunicationModeOn", RN.NativeModules.NativeAudioManagerModule === null ? RN.NativeModules.RTNAudioManager : RN.NativeModules.NativeAudioManagerModule, () => {});
+export const onUnload = instead("setCommunicationModeOn", RNReactNative.TurboModuleRegistry.get("NativeAudioManagerModule") === null ? RN.TurboModuleRegistry.get("RTNAudioManager") : RN.TurboModuleRegistry.get("NativeAudioManagerModule"), () => {});
